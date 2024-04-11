ef50cd75c6ca3f1982462b8c3ca910fed52f8404
Jacfger <ncfung@connect.ust.hk>
2024-04-11 22:26:14 +0800

    .

--- cpp/53_maximum_subarray.cpp
+++ cpp/53_maximum_subarray.cpp
@@ -1,23 +1,18 @@
 #include "helper.h"
-class Solution
-{
-public:
-    int maxSubArray(vector<int> &nums)
-    {
+class Solution {
+   public:
+    int maxSubArray(vector<int> &nums) {
         vector<int> nums2;
         nums2.push_back(nums[0]);
-        for (int i = 1; i < nums.size(); ++i)
-        {
+        for (int i = 1; i < nums.size(); ++i) {
             nums2.push_back(nums[i] + nums2[i - 1]);
         }
         int mnum = 100000;
         int s = 0;
         int maxsum = nums[0];
-        for (int i = 0; i < nums2.size(); ++i)
-        {
+        for (int i = 0; i < nums2.size(); ++i) {
             maxsum = max(maxsum, nums2[i] - mnum);
-            if (nums2[i] < mnum)
-            {
+            if (nums2[i] < mnum) {
                 mnum = nums2[i];
                 s = i;
             }
@@ -26,8 +21,7 @@ public:
     }
 };
 
-int main()
-{
+int main() {
     Solution solution;
     vector<int> nums = {1, 2, -1};
     cout << solution.maxSubArray(nums) << endl;
--- cpp/helper.h
+++ cpp/helper.h
@@ -3,51 +3,72 @@
 
 #include <algorithm>
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <ranges>
-using namespace std;
-
-template <std::ranges::range T>
-void _print(T const &, char='\n');
+#include <vector>
 
 template <typename T>
-void _print(T const &, char='\n');
+void _print(T const &variable) {
+    std::cout << variable;
+}
+
+template <std::size_t N>
+void _print(char const (&variable)[N]) {
+    std::cout << variable;
+}
 
 template <std::ranges::range T>
-void _print(T const &iteratable, char end)
-{
+void _print(T const &iteratable) {
     std::cout << '{';
-    for (auto it = iteratable.begin(); it != iteratable.end(); ++it)
-    {
-        if (it != iteratable.begin())
-        {
+    for (auto it = iteratable.begin(); it != iteratable.end(); ++it) {
+        if (it != iteratable.begin()) {
             std::cout << ", ";
         }
-        _print(*it, 0);
+        _print(*it);
     }
     std::cout << '}';
-    if (end == '\n')
-    {
-        std::cout << std::endl;
-    }
-    else
-    {
-        std::cout << end;
-    }
 }
 
-template <typename T>
-void _print(T const &variable, char end)
-{
-    std::cout << variable;
-    if (end == '\n')
-    {
-        std::cout << std::endl;
+// template <std::ranges::range T>
+// auto operator<<(std::ostream &stream, T const &c) {
+//     return stream << "int container\n";
+// }
+template <std::size_t N = 2, std::size_t M = 2>
+struct print_options {
+    constexpr print_options(const char (&_sep)[N], const char (&_end)[M]) {
+        std::copy(_sep, _sep + N, sep);
+        std::copy(_end, _end + M, end);
     }
-    else
-    {
-        std::cout << end;
-    }
+    char sep[N];
+    char end[M];
+};
+
+// template <typename... Args>
+// auto print(const Args &...args) {
+//     // lambda used as decorator to add a space
+//     // expands to ( ( ( std::cout << v1 ) << v2 ) << ... )
+//     (std::cout << ... << [&](const auto &ref) -> char {
+//         std::cout << ref;
+//         return ' ';
+//     }(args))
+//         << '\n';
+// }
+// template <size_t N, typename... U>
+// void print_with_sep(const char (&sep), U &&...args) {
+//     (std::cout << ... << (o.sep, _print(args)));
+// }
+
+template <print_options o = print_options{"a", "\n"}, typename T, typename... U>
+void print(const T &first, const U &...rest) {
+    _print(first);
+    // (std::cout << o.sep << rest, ...);
+    (std::cout << ... << (o.sep, _print(rest)));
+    // (std::cout << ... << [&](const auto &s) {
+    //     _print(s);
+    //     return o.sep;
+    // }(rest));
+    std::cout << o.end;
 }
 
+using namespace std;
 #endif
